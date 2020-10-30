e6186442b5987b5de5d51f9af48c39c029efcbae
PonFET <tomas.ap@live.com>
2020-10-30 01:18:00 -0300

    JSON a BD
    Seteo de config, se sumaron DAOs de conexion de BD, se paso persistencia de JSON a BD. Seccion de generos y peliculas quedo rota.

--- dao/iuserdao.php
+++ dao/iuserdao.php
@@ -7,7 +7,7 @@
     {
         function Register(User $user);
         function GetAll();
-        function ChangeType(User $user, $type);
+        //function ChangeType(User $user, $type);
         function SearchUser($name);
         function VerifyPassword(User $user);
         function FullUser(User $user);
